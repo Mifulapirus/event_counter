34aab8152508a35babcc70ae0111f50455107be6
Ángel <angel@tupperbot.com>
2021-03-09 00:28:01 +0100

    first commit

---
+++ event_counter - fw/src/Config.h
@@ -0,0 +1,92 @@
+#ifndef SPIFFS
+    #include <FS.h>
+#endif // !SPIFFS
+#ifndef logger
+    #include <Logger.h>
+#endif // !logger
+#ifndef ArduinoOTA
+    #include <ArduinoOTA.h>
+#endif // !ArduinoOTA
+
+
+#include <ArduinoJson.h>
+
+
+/**
+ * Config helper.
+ * This structure contains all variables related to the configuration of this firmware
+ */
+struct Config {
+  bool using_congfig_file;
+  char version[8] = __VERSION__;
+  char ap_ssid[64] = "ESP WiFi";
+  char device_name[20] = "ESP thing";
+  char ap_ip[20] = "10.0.1.1";
+  char ap_gateway[20] = "10.0.1.1";
+  char ap_mask[20] = "255.255.255.0";
+  const char compilation_date[30] = __DATE__ " + " __TIME__;
+};
+
+Config config;                         //Create global configuration object
+
+
+/**
+ * Prints the configuration file read from memory
+ * @param filename Name of the configuration file including the .json extension
+ **/
+void printConfigFile(const char *filename) {
+  // Open file for reading
+  File file = SPIFFS.open(filename, "r");
+  if (!file) {
+    logger(F("Failed to read file"));
+    return;
+  }
+
+  // Extract each characters by one by one
+  while (file.available()) logger(file.readString());
+  file.close();
+}
+
+/**
+ * Loads given configuration file into the appropriate config object
+ * @param filename Name of the configuration file, including .json extension
+ * @param &config Address pointer to the config object
+ **/
+void loadConfiguration(const char *filename, Config &config) {
+    File file = SPIFFS.open(filename, "r");
+    StaticJsonDocument<512> doc;
+
+    // Deserialize the JSON document
+    DeserializationError error = deserializeJson(doc, file);
+    if (error)
+        logger(F("Failed to read config file, using default configuration"));
+
+    // Copy values from the JsonDocument to the Config
+    strlcpy(config.version,           // <- destination
+            doc["version"] | "0.0",   // <- source
+            sizeof(config.version));  // <- destination's capacity
+    strlcpy(config.device_name, doc["device_name"] | "No Name (Default)", sizeof(config.device_name));          
+    strlcpy(config.ap_ssid, doc["ap_ssid"] | "ESP Thing (Default)", sizeof(config.ap_ssid));  
+    strlcpy(config.ap_ip, doc["ap_ip"] | "10.0.1.1", sizeof(config.ap_ip));  
+    strlcpy(config.ap_gateway, doc["ap_gateway"] | "10.0.1.1", sizeof(config.ap_gateway));  
+    strlcpy(config.ap_mask, doc["ap_path"] | "255.255.255.0", sizeof(config.ap_mask));  
+}
+
+void OTAsetup(){
+    ArduinoOTA.setHostname(config.device_name);
+    logger("  Hostname: " + ArduinoOTA.getHostname());
+
+    ArduinoOTA.onStart([]() {logger("\n*************************\n****** OTA STARTED ******");});
+    ArduinoOTA.onEnd([]() {logger("\n*************************\n****** OTA FINISHED ******");});
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {Serial.printf("*** OTA Progress: %u%%\r", (progress / (total / 100)));});
+    ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("OTA Error[%u]: \n", error);
+        if (error == OTA_AUTH_ERROR) logger("OTA Error: Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) logger("OTA Error: Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) logger("OTA Error: Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) logger("OTA Error: Receive Failed");
+        else if (error == OTA_END_ERROR) logger("OTA Error: End Failed");
+    });
+
+    ArduinoOTA.begin();
+}
---
+++ event_counter - fw/src/main.cpp
@@ -0,0 +1,141 @@
+/*************************************************************************************************************
+ * <PROJECT NAME>
+ * Description: 
+ * Author: Angel Hernandez
+ * Contact: angel@gaubit.com
+ * 
+ * Description: 
+ *  
+ *
+ **************************************************************************************************************/
+#include <Arduino.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <Logger.h>
+#include <ArduinoJson.h>
+#include <FS.h>
+#include <HTTPSRedirect.h>
+
+
+//OTA related libraries
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+#include <Config.h>
+
+
+const char *configPath = "/config.json";  
+
+const char compile_date[] = __DATE__ " " __TIME__;
+
+// The ID below comes from Google Sheets.
+// Towards the bottom of this page, it will explain how this can be obtained
+//https://script.google.com/macros/s/AKfycbz6JCyGbPDGMm7WRX6xyR1hZJiI9wRnIfUh5r69cYs7asQfy46fzqBjWrKHaHXGapjKiA/exec
+const char *GScriptId = "AKfycbz6JCyGbPDGMm7WRX6xyR1hZJiI9wRnIfUh5r69cYs7asQfy46fzqBjWrKHaHXGapjKiA";
+const char* host = "script.google.com";
+const char* googleRedirHost = "script.googleusercontent.com";
+const int httpsPort =     443;
+HTTPSRedirect client(httpsPort);
+
+// Prepare the url (without the varying data)
+String url = String("/macros/s/") + GScriptId + "/exec?";
+
+const char* fingerprint = "F0 5C 74 77 3F 6B 25 D7 3B 66 4D 43 2F 7E BC 5B E9 28 86 AD";
+
+// This is the main method where data gets pushed to the Google sheet
+void postData(String tag, int value){
+    if (!client.connected()){
+            Serial.println("Connecting to client again…");
+            client.connect(host, httpsPort);
+    }
+    String urlFinal = url + "tag=" + tag + "&value=" + String(value);
+    client.GET(urlFinal, host, googleRedirHost);
+}
+
+/**
+ * Perform all the required set up before main loop starts 
+ **/
+void setup() {
+  Serial.begin(115200);
+  //Mount File system
+  if (initLogger(true)) logger("\nFS mounted at setup");
+  else logger("Error mounting FS at setup");
+  //Log the beginning of the program
+  logger("\n\n-----------START----------");
+  logger("| Compilation Date: " + String(compile_date));
+  logger("| OTA Active");
+
+  //Logger status
+  logger("| Initialize Current Log file: " + String(clearCurrentLogFile()));
+
+  //Load config
+  logger(F("| Print config file..."));
+  printConfigFile(configPath);
+  logger(F("| Loading configuration..."));
+  loadConfiguration(configPath, config);
+
+  //WiFiManager
+  WiFiManager wifiManager;
+  
+  //reset saved settings
+  //wifiManager.resetSettings();
+  
+  //set custom ip for portal
+  IPAddress ip, gw, mask;
+  ip.fromString(config.ap_ip);
+  gw.fromString(String(config.ap_gateway));
+  mask.fromString(String(config.ap_mask));
+  
+  WiFi.hostname(config.device_name);
+
+  wifiManager.setAPStaticIPConfig(ip, gw, mask);
+
+  //fetches ssid and pass from eeprom and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  wifiManager.autoConnect(config.ap_ssid);
+
+  logger("  " + String(config.device_name) + ": " + String(config.version));
+  logger("  Host Name: " + String(WiFi.hostname()));
+  logger("  Hardware MAC: " + String(WiFi.macAddress()));
+  logger("  Software MAC: " + WiFi.softAPmacAddress());
+  logger("  IP: " + WiFi.localIP().toString());  
+  OTAsetup();  
+
+  logger("| Initiate TLS connection");
+  client.setInsecure();
+  bool connected = false;
+  for (int i=0; i<5; i++){
+    int retval = client.connect(host, httpsPort);
+    if (retval == 1) {
+      connected = true;
+      break;
+    }
+    else Serial.println("Connection failed. Retrying…");
+  }
+
+  // Connection Status, 1 = Connected, 0 is not.
+  Serial.println("Connection Status: " + String(client.connected()));
+  Serial.flush();
+  
+  if (!connected){
+    Serial.print("Could not connect to server: ");
+    Serial.println(host);
+    Serial.println("Exiting…");
+    Serial.flush();
+    return;
+  }
+
+  // Data will still be pushed even certification don’t match.
+  if (client.verify(fingerprint, host)) {
+    Serial.println("Certificate match.");
+  } 
+  else {
+    Serial.println("Certificate mis-match");
+  }
+
+  logger("-------Setup Finished-------");
+}
+
+void loop() {
+  ArduinoOTA.handle();
+}
